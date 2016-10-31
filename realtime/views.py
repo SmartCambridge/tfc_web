04a41b00ca1ede706b3bd8ead9d44f7cda193b98
Abraham Martin <abraham@watermac.local>
2016-10-31 07:47:54 +0000

    Add timestamp to title

--- realtime/views.py
+++ realtime/views.py
@@ -4,12 +4,7 @@ from django.shortcuts import render
 
 
 def home(request):
-    bus_data = requests.get('http://smartcambridge.org/backdoor/dataserver/raw/file/monitor_json/post_data.json')
-    bus_data_json = bus_data.json()
-
-    return render(request, 'home.html', {
-        'bus_locations': bus_data_json,
-    })
+    return render(request, 'home.html', {})
 
 
 def busdata_json(request):
