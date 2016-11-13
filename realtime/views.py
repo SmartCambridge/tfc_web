1abee71eb3155014f514df524800e913d0f940c5
Abraham Martin <abraham@watermac.local>
2016-11-13 12:46:26 +0000

    added BusStop model
    It will be used as a cache of the bus stop data from DFT
    (http://naptan.app.dft.gov.uk/datarequest/help)

--- realtime/views.py
+++ realtime/views.py
@@ -10,4 +10,3 @@ def home(request):
 def busdata_json(request):
     bus_data = requests.get('http://smartcambridge.org/backdoor/dataserver/raw/file/monitor_json/post_data.json')
     return JsonResponse(bus_data.json())
-
