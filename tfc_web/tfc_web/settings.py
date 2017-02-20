ba27173cecb9fab3cfdd052c4a8573ca1a6ea2bd
Abraham Martin <abraham.martin@gmail.com>
2017-02-20 23:03:57 +0000

    change app name to avoid conflict

--- tfc_web/tfc_web/settings.py
+++ tfc_web/tfc_web/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.gis',
-    'gis',
+    'tfc_gis',
     'transport',
     'vix',
     'parking',
