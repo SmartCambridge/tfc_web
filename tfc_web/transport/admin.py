bbc7efb1a728f18e5615ef082b62f667aeb2d5ad
Abraham Martin <abraham.martin@gmail.com>
2017-02-21 19:55:31 +0000

    gis admin backend

--- tfc_web/transport/admin.py
+++ tfc_web/transport/admin.py
@@ -1,11 +1,12 @@
-from django.contrib.gis import admin
-from django.contrib.gis.admin import ModelAdmin
+from django.contrib import admin
+from django.contrib.admin import ModelAdmin
+from django.contrib.gis.admin import ModelAdmin as GISModelAdmin
 from transport.models import Line, Stop, Operator, Route, VehicleJourney, JourneyPatternSection, \
     JourneyPattern
 
 
 @admin.register(Stop)
-class StopAdmin(admin.ModelAdmin):
+class StopAdmin(GISModelAdmin):
     list_display = ('atco_code', 'common_name', 'locality_name')
 
 
@@ -14,4 +15,5 @@ admin.site.register(Operator, ModelAdmin)
 admin.site.register(Line, ModelAdmin)
 admin.site.register(JourneyPatternSection, ModelAdmin)
 admin.site.register(JourneyPattern, ModelAdmin)
+admin.site.register(VehicleJourney, ModelAdmin)
 admin.site.register(VehicleJourney, ModelAdmin)
