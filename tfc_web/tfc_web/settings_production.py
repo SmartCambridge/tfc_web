5fb86db9ba30da1d9810746d216490a5c7bb4a04
Abraham Martin <abraham.martin@gmail.com>
2017-03-06 22:22:44 +0000

    update settings files

--- tfc_web/tfc_web/settings_production.py
+++ tfc_web/tfc_web/settings_production.py
@@ -1,15 +1,3 @@
 from tfc_web.settings import *
-from tfc_web.secrets import DATABASE_PASSWORD
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': 'tfcweb',
-        'USER': 'tfcwebuser',
-        'PASSWORD': DATABASE_PASSWORD,
-        'HOST': 'localhost',
-        'PORT': '',
-    }
-}
-
-API_ENDPOINT = 'http://smartcambridge.org'
+DEBUG = False
