0c8c66f80f9e92dae31d9ee722b9ae8b3ec31bc3
Abraham Martin <abraham.martin@gmail.com>
2017-09-21 16:56:43 +0100

    added production email configuration

--- tfc_web/tfc_web/settings_production.py
+++ tfc_web/tfc_web/settings_production.py
@@ -2,4 +2,8 @@ from tfc_web.settings import *
 
 DEBUG = False
 
+DEFAULT_FROM_EMAIL = "smart-cambridge@cl.cam.ac.uk"
+EMAIL_HOST = "ppsw.cam.ac.uk"
+EMAIL_PORT = 25
+
 TFC_SERVER_CSN_API = "http://localhost/httpmsg/A/tfc.manager/msgrouter/A"
