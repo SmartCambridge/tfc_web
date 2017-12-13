e870ba612eae73eb31339a3629ee0e23f6d6f4b3
Ian Lewis <ijl20@cam.ac.uk>
2017-12-13 14:55:49 +0000

    Minor changes to map_template and busdata-json to support vix2 feed

--- tfc_web/transport/views.py
+++ tfc_web/transport/views.py
@@ -41,9 +41,9 @@ def busdata_json(request):
     else:
         boundaries_enabled = False
 
-    bus_data = requests.get(settings.API_ENDPOINT+'/api/dataserver/feed/now/vix').json() \
+    bus_data = requests.get(settings.API_ENDPOINT+'/api/dataserver/feed/now/vix2').json() \
         if 'previous' not in request.GET else \
-        requests.get(settings.API_ENDPOINT+'/api/dataserver/feed/previous/vix').json()
+        requests.get(settings.API_ENDPOINT+'/api/dataserver/feed/previous/vix2').json()
     if boundaries_enabled:
         for index, bus in enumerate(bus_data['request_data']['entities']):
             if north > bus['latitude'] > south and west < bus['longitude'] < east:
