a4f581bd1a66cc214818892a69896ab219c301fc
Abraham Martin <amc203@cam.ac.uk>
2018-01-21 21:12:02 +0000

    added timetable to the return of the API

--- tfc_web/transport/api/serializers.py
+++ tfc_web/transport/api/serializers.py
@@ -3,6 +3,11 @@ from transport.models import VehicleJourney
 
 
 class VehicleJourneySerializer(serializers.ModelSerializer):
+    timetable = serializers.SerializerMethodField()
+
+    def get_timetable(self, obj):
+        return obj.get_timetable().values('order', 'stop_id', 'time')
+
     class Meta:
         model = VehicleJourney
         fields = '__all__'
