409eae569ea5400bba06a6100e481c8f99d4b421
Abraham Martin <amc203@cam.ac.uk>
2018-01-26 23:03:24 +0000

    improve journeys_by_time_and_stop API

--- tfc_web/transport/api/serializers.py
+++ tfc_web/transport/api/serializers.py
@@ -4,14 +4,9 @@ from transport.models import VehicleJourney, Line, Stop, SpecialDaysOperation
 
 class VehicleJourneySerializer(serializers.ModelSerializer):
     timetable = serializers.SerializerMethodField()
-    special_days_operation = serializers.SerializerMethodField()
 
     def get_timetable(self, obj):
         return obj.get_timetable().values('order', 'stop_id', 'time')
-
-    def get_special_days_operation(self, obj):
-        return SpecialDaysOperation.objects.filter(vehicle_journey=obj)\
-            .values('days__lower', 'days__upper', 'operates')
 
     class Meta:
         model = VehicleJourney
