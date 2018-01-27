505c6144fe5057d9c7beca2205b10220e52e9131
Abraham Martin <abraham.martin@gmail.com>
2018-01-27 11:17:53 +0000

    add stop info to timetable serialiser

--- tfc_web/transport/api/serializers.py
+++ tfc_web/transport/api/serializers.py
@@ -1,12 +1,12 @@
 from rest_framework import serializers
-from transport.models import VehicleJourney, Line, Stop, SpecialDaysOperation
+from transport.models import VehicleJourney, Line, Stop, Timetable
 
 
 class VehicleJourneySerializer(serializers.ModelSerializer):
     timetable = serializers.SerializerMethodField()
 
     def get_timetable(self, obj):
-        return obj.get_timetable().values('order', 'stop_id', 'time')
+        return TimetableSerializerForJourney(obj.get_timetable(), many=True).data
 
     class Meta:
         model = VehicleJourney
@@ -25,4 +25,16 @@ class StopSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Stop
-        fields = '__all__'
+        fields = ['atco_code', 'naptan_code', 'common_name', 'indicator', 'locality_name', 'longitude', 'latitude']
+
+
+class TimetableSerializerForJourney(serializers.ModelSerializer):
+    stop = serializers.SerializerMethodField()
+
+    def get_stop(self, obj):
+        return StopSerializer(obj.stop).data
+
+    class Meta:
+        model = Timetable
+        fields = ['order', 'stop', 'time']
+        depth = 2
