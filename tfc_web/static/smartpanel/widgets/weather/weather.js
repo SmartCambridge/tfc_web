1d90ed4b7d942965205fdbca74a1344c5bda0dd1
Jon Warbrick <jw35@cam.ac.uk>
2018-03-23 14:55:25 +0000

    Import latest weather widget from acp_lobby_screen
    Imported and adapted the latest version of the weather widget from
    the acp_lobby_screen repository. Futher tweaked the framework to
    get it to work.
    
    Introduced the concept of the widget area automatically being in a class
    named after the widget it contains.

--- tfc_web/static/smartpanel/widgets/weather/weather.js
+++ tfc_web/static/smartpanel/widgets/weather/weather.js
@@ -1,17 +1,26 @@
 /* Weather Widget for ACP Lobby Screen */
 
-/*global $ */
+/* global $, DEBUG */
+/* exported Weather */
 
-function Weather(container, params, static_url) {
+function Weather(config, params) {
 
     'use strict';
 
+    // Backwards compatibility or first argument
+    var container;
+    if (typeof(config) === 'string') {
+        container = config;
+    }
+    else {
+        this.config = config;
+        container = config.container;
+    }
     this.container = container;
     this.params = params;
-    this.static_url = static_url;
 
     this.init = function () {
-        this.log("Running init", this.container);
+        this.log('Running init', this.container);
         this.do_load();
     };
 
@@ -21,15 +30,15 @@ function Weather(container, params, static_url) {
     }*/
 
     this.do_load = function () {
-        this.log("Running Weather.do_load", this.container);
+        this.log('Running Weather.do_load', this.container);
         var self = this,
-            url = "../../weather?location=" + this.params.location +
-                " .weather";
-        this.log("do_load URI", url);
-        this.log("Container", '#' + this.container);
+            url = '../../weather?location=' + this.params.location +
+                ' .content_area';
+        this.log('do_load URI', url);
+        this.log('Container', '#' + this.container);
         $('#' + this.container).load(url, function (response, status, xhr) {
             if (status === 'error') {
-                self.log("Error loading station board", xhr.status, xhr.statusText);
+                self.log('Error loading station board', xhr.status, xhr.statusText);
                 $('#' + self.container + ' .widget_error').show();
             }
             else {
@@ -37,7 +46,7 @@ function Weather(container, params, static_url) {
             }
             setTimeout(function () { self.do_load(); }, 60000);
         });
-        this.log("do_load done", this.container);
+        this.log('do_load done', this.container);
     };
 
     this.log = function() {
@@ -46,6 +55,6 @@ function Weather(container, params, static_url) {
         }
     };
 
-    this.log("Instantiated Weather", container, params);
+    this.log('Instantiated Weather', container, params);
 
 }
