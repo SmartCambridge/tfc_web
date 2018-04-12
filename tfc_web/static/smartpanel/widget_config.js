c42c9ed30134594a918c7bdaf5dbdc5fa7a49798
ijl20 <ijl20@cam.ac.uk>
2018-04-12 14:57:31 +0100

    config_widget now separate JS and CSS files widget_config.js widget_config.css

--- tfc_web/static/smartpanel/widget_config.js
+++ tfc_web/static/smartpanel/widget_config.js
@@ -1,179 +1,166 @@
-// ************************************************************************************
-// *****************  Library Widget Configuration ************************************
-// ************************************************************************************
-//
-// THESE ARE GENERAL PURPOSE CONFIG INPUT FUNCTIONS, should be in common widget.js
-//
-// config_input will append a <tr> to the parent object 'parent_el'
-// and add an entry to the global dictionary:
-// config_inputs[param_name] = { type: param_type,
-//                               options: param_options,
-//                               value: a function that returns the value }
-//
-// config_input(
-//   parent_el:     DOM object to append input element (tbody)
-//   param_name:    property name for widget, e.g. 'station'.
-//   param_type:    'select' | 'string' | 'number'
-//   param_options: options needed for each input type
-//   param_current: current value of property (for edit)
-//   )
-// 'select': { text: text display before dropdown
-//             title: helper text
-//             options: [ { value: <key>, text: <displayname> } ... ]
-//            }
-//
-// 'string':  { text:
-//              title:
-//            });
-//
-// 'number':  { text:
-//              title:
-//              step: 'any' *[OPTIONAL]
-//            });
-function config_input(parent_el, param_name, param_type, param_options, param_current) {
-    self.log('creating input', param_name, param_type, param_options, param_current);
-    config_inputs[param_name] = {};
-    config_inputs[param_name].type = param_type;
-    config_inputs[param_name].options = param_options;
-    switch (param_type) {
-        case 'select':
-            parent_el.appendChild(config_select(param_name, param_options, param_current));
-            break;
 
-        case 'string':
-            parent_el.appendChild(config_string(param_name, param_options, param_current));
-            break;
 
-        case 'number':
-            parent_el.appendChild(config_number(param_name, param_options, param_current));
-            break;
+    // ************************************************************************************
+    // *****************  Library Widget Configuration ************************************
+    // ************************************************************************************
+    //
+    // THESE ARE GENERAL PURPOSE CONFIG INPUT FUNCTIONS, should be in common widget.js
+    //
+    // config_input will append a <tr> to the parent object 'parent_el'
+    // and add an entry to the global dictionary:
+    // config_inputs[param_name] = { type: param_type,
+    //                               options: param_options,
+    //                               value: a function that returns the value }
+    //
+    // config_input(
+    //   parent_el:     DOM object to append input element (tbody)
+    //   param_name:    property name for widget, e.g. 'station'.
+    //   param_type:    'select' | 'string' | 'number'
+    //   param_options: options needed for each input type
+    //   param_current: current value of property (for edit)
+    //   )
+    // 'select': { text: text display before dropdown
+    //             title: helper text
+    //             options: [ { value: <key>, text: <displayname> } ... ]
+    //            }
+    //
+    // 'string':  { text:
+    //              title:
+    //            });
+    //
+    // 'number':  { text:
+    //              title:
+    //              step: 'any' *[OPTIONAL]
+    //            });
+    function config_input(parent_el, param_name, param_type, param_options, param_current) {
+        //self.log('creating input', param_name, param_type, param_options, param_current);
+        var input_info = {}; // info to return, .value() = data callback
+        switch (param_type) {
+            case 'select':
+                input_info.value = config_select(parent_el, param_name, param_options, param_current);
+                break;
 
-        default:
-            self.log(widget_id, 'bad param_type in config_input', param_type);
+            case 'string':
+                input_info.value = config_string(parent_el, param_name, param_options, param_current);
+                break;
+
+            case 'number':
+                input_info.value = config_number(parent_el, param_name, param_options, param_current);
+                break;
+
+            default:
+                input_info = null;
+                //self.log(widget_id, 'bad param_type in config_input', param_type);
+        }
+
+        return input_info;
     }
 
-    return null;
-}
+    function config_select(parent_el, param_name, param_options, param_current) {
+        //self.log('creating select element', param_name, 'with', param_current);
+        //var id = config_id + '_' + param_name;
+        var row = document.createElement('tr');
 
-function config_select(param_name, options, param_current) {
-    self.log('creating select element', param_name, 'with', param_current);
-    var id = config_id + '_' + param_name;
-    var row = document.createElement('tr');
+        // create td to hold 'name' prompt for field
+        var name = document.createElement('td');
+        name.className = 'widget_config_property_name';
+        var label = document.createElement('label');
+        //label.htmlFor = id;
+        label.title = param_options.title;
+        label.appendChild(document.createTextNode(param_options.text));
+        name.appendChild(label);
+        row.appendChild(name);
+        var value = document.createElement('td');
+        value.className = 'config_property_value';
+        var sel = document.createElement('select');
+        if (param_options.title) sel.title = param_options.title;
+        //sel.id = id;
+        var select_options = param_options.options;
+        for (var i=0; i<select_options.length; i++) {
+            var opt = document.createElement('option');
+            opt.value = select_options[i].value;
+            opt.text = select_options[i].text;
+            sel.appendChild(opt);
+        }
+        // set default value of input to value provided in param_current
+        if (param_current) sel.value = param_current;
+        //config_inputs[param_name].element = sel; // add input element to global dict for Save
+        value.appendChild(sel);
+        row.appendChild(value);
+        parent_el.appendChild(row);
 
-    // create td to hold 'name' prompt for field
-    var name = document.createElement('td');
-    name.class = 'config_property_name';
-    var label = document.createElement('label');
-    label.for = id;
-    label.title = options.title;
-    label.appendChild(document.createTextNode(options.text));
-    name.appendChild(label);
-    row.appendChild(name);
-    var value = document.createElement('td');
-    value.class = 'config_property_value';
-    var sel = document.createElement('select');
-    if (options.title) sel.title = options.title;
-    sel.id = id;
-    var select_options = options.options;
-    for (var i=0; i<select_options.length; i++) {
-        var opt = document.createElement('option');
-        opt.value = select_options[i].value;
-        opt.text = select_options[i].text;
-        sel.appendChild(opt);
+        return function () { return sel.value; };
     }
-    // set default value of input to value provided in param_current
-    if (param_current) sel.value = param_current;
-    config_inputs[param_name].element = sel; // add input element to global dict for Save
-    config_inputs[param_name].value = function () { return sel.value; };
-    value.appendChild(sel);
-    row.appendChild(value);
 
-    return row;
-}
-
-function config_number(param_name, param_options, param_current) {
-    if (!param_options.type) param_options.type = 'number';
-    return config_string(param_name, param_options, param_current);
-}
-
-// Return a table row with a simple input field
-function config_string(param_name, options, param_current)
-{
-    var id = config_id + '_' + param_name;
-    var row = document.createElement('tr');
-    // create td to hold 'name' prompt for field
-    var name = document.createElement('td');
-    name.class = 'config_property_name';
-    var label = document.createElement('label');
-    label.for = id;
-    label.title = options.title;
-    label.appendChild(document.createTextNode(options.text));
-    name.appendChild(label);
-    row.appendChild(name);
-    var value = document.createElement('td');
-    value.class = 'config_property_value';
-
-    var input = document.createElement('input');
-    input.id = id;
-    if (options.type) input.type = options.type;
-    if (options.step) input.step = options.step;
-    if (options.title) input.title = options.title;
-
-    // set default value of input to value provided in param_current
-    self.log(param_name,'default set to',param_current);
-    if (param_current) input.value = param_current;
-
-    config_inputs[param_name].element = input;
-    config_inputs[param_name].value = function() { return input.value; };
-    value.appendChild(input);
-    row.appendChild(value);
-
-    return row;
-}
-
-// A 'shim' callback for the configuration results
-function config_shim_callback(config_params) {
-    if (config_params) {
-        config_shim_save(config_params);
-    } else {
-        config_shim_cancel();
+    function config_number(parent_el, param_name, param_options, param_current) {
+        if (!param_options.type) param_options.type = 'number';
+        return config_string(parent_el, param_name, param_options, param_current);
     }
-}
 
-// A shim function to provide the 'config cancel' in the active layout
-function config_shim_cancel() {
-    // **                                                         **
-    // reset original widget background-color to WHITE
-    var widget = document.getElementById(widget_id);
-    if (widget) widget.style['background-color'] = 'white';
+    // Return a table row with a simple input field
+    function config_string(parent_el, param_name, param_options, param_current)
+    {
+        var row = document.createElement('tr');
+        // create td to hold 'name' prompt for field
+        var name = document.createElement('td');
+        name.className = 'widget_config_property_name';
+        var label = document.createElement('label');
+        //label.htmlFor = id;
+        label.title = param_options.title;
+        label.appendChild(document.createTextNode(param_options.text));
+        name.appendChild(label);
+        row.appendChild(name);
+        var value = document.createElement('td');
+        value.className = 'config_property_value';
 
-    // hide the config div
-    var config = document.getElementById(config_id);
-    config.style.display = 'none';
-    // **                                                         **
-    // *************************************************************
+        var input = document.createElement('input');
 
-    self.log(config_id, 'cancel button');
-}
+        if (param_options.type) input.type = param_options.type;
+        if (param_options.step) input.step = param_options.step;
+        if (param_options.title) input.title = param_options.title;
 
-// A shim function to provide the 'config save' in the active layout
-function config_shim_save(config_params) {
-    // Here we update the existing widget 'in-place', not expected in production
+        // set default value of input to value provided in param_current
+        //self.log(param_name,'default set to',param_current);
+        if (param_current) input.value = param_current;
 
-    params = config_params; //self.params = config_params;
+        value.appendChild(input);
+        row.appendChild(value);
 
-    self.log(widget_id,'config reset params to',params);//self.params);
+        parent_el.appendChild(row);
 
-    var widget = document.getElementById(widget_id);
+        return function() { return input.value; };
+    }
 
-    // reset original widget background-color to WHITE
-    widget.style['background-color'] = 'white';
+    // A shim function to provide the 'config cancel' in the active layout
+    function config_shim_cancel(self, config) {
+        // reset original widget background-color to WHITE
+        var widget = document.getElementById(config.widget_id);
+        if (widget) widget.style['background-color'] = 'white';
 
-    // hide the config div
-    var config = document.getElementById(config_id);
-    config.style.display = 'none';
+        // hide the config div
+        var config_div = document.getElementById(config.config_id);
+        config_div.style.display = 'none';
 
-    self.init();
-}
+        self.log(config.config_id, 'cancel button');
+    }
 
+    // A shim function to provide the 'config save' in the active layout
+    function config_shim_save(self, config, config_params) {
+        // Here we update the existing widget 'in-place', not expected in production
+
+        self.params = config_params; //self.params = config_params;
+
+        self.log(config.widget_id,'config reset params to',self.params);//self.params);
+
+        var widget = document.getElementById(config.widget_id);
+
+        // reset original widget background-color to WHITE
+        widget.style['background-color'] = 'white';
+
+        // hide the config div
+        var config_div = document.getElementById(config.config_id);
+        config_div.style.display = 'none';
+
+        self.init();
+    }
 
--- tfc_web/static/smartpanel/widgets/station_board/station_board.js
+++ tfc_web/static/smartpanel/widgets/station_board/station_board.js
@@ -29,10 +29,10 @@ function StationBoard(config, params) {
 
     var config_inputs = {}; // DOM id's (station, offset) for params form elements
 
-    var params = params;
+    self.params = params;
 
     this.init = function () {
-        this.log("Running init", widget_id, 'with params', params );
+        this.log("Running init", widget_id, 'with params', self.params );
 
         // debug - add a 'configure' link to the bottom of the page
         // and create an initially hidden config div for the widget to use.
@@ -81,9 +81,9 @@ function StationBoard(config, params) {
     this.do_load = function () {
         this.log("Running StationBoard.do_load", widget_id);
         //var self = this;
-        var url = "/smartpanel/station_board?station=" + params.station;
-        if (params.offset) { //(this.params.offset) {
-            url += "&offset=" + params.offset;
+        var url = "/smartpanel/station_board?station=" + self.params.station;
+        if (self.params.offset) {
+            url += "&offset=" + self.params.offset;
         }
         url += " .content_area";
 
@@ -112,7 +112,7 @@ function StationBoard(config, params) {
                           config_id: config_id,
                           configuration_callback: config_shim_callback
                         },
-                        params);
+                        self.params);
     }
 
     this.log = function() {
@@ -212,7 +212,7 @@ function StationBoard(config, params) {
 
     // user has clicked the 'Save' button
     function config_click_save(config, config_inputs) {
-        self.log(config_id, 'save_button');
+        self.log(config.config_id, 'save_button');
 
         var config_params = {};
         // station
@@ -238,170 +238,6 @@ function StationBoard(config, params) {
         } else {
             config_shim_cancel(self, config);
         }
-    }
-
-    // ************************************************************************************
-    // *****************  Library Widget Configuration ************************************
-    // ************************************************************************************
-    //
-    // THESE ARE GENERAL PURPOSE CONFIG INPUT FUNCTIONS, should be in common widget.js
-    //
-    // config_input will append a <tr> to the parent object 'parent_el'
-    // and add an entry to the global dictionary:
-    // config_inputs[param_name] = { type: param_type,
-    //                               options: param_options,
-    //                               value: a function that returns the value }
-    //
-    // config_input(
-    //   parent_el:     DOM object to append input element (tbody)
-    //   param_name:    property name for widget, e.g. 'station'.
-    //   param_type:    'select' | 'string' | 'number'
-    //   param_options: options needed for each input type
-    //   param_current: current value of property (for edit)
-    //   )
-    // 'select': { text: text display before dropdown
-    //             title: helper text
-    //             options: [ { value: <key>, text: <displayname> } ... ]
-    //            }
-    //
-    // 'string':  { text:
-    //              title:
-    //            });
-    //
-    // 'number':  { text:
-    //              title:
-    //              step: 'any' *[OPTIONAL]
-    //            });
-    function config_input(parent_el, param_name, param_type, param_options, param_current) {
-        self.log('creating input', param_name, param_type, param_options, param_current);
-        var input_info = {}; // info to return, .value() = data callback
-        switch (param_type) {
-            case 'select':
-                input_info.value = config_select(parent_el, param_name, param_options, param_current);
-                break;
-
-            case 'string':
-                input_info.value = config_string(parent_el, param_name, param_options, param_current);
-                break;
-
-            case 'number':
-                input_info.value = config_number(parent_el, param_name, param_options, param_current);
-                break;
-
-            default:
-                input_info = null;
-                self.log(widget_id, 'bad param_type in config_input', param_type);
-        }
-
-        return input_info;
-    }
-
-    function config_select(parent_el, param_name, param_options, param_current) {
-        self.log('creating select element', param_name, 'with', param_current);
-        //var id = config_id + '_' + param_name;
-        var row = document.createElement('tr');
-
-        // create td to hold 'name' prompt for field
-        var name = document.createElement('td');
-        name.className = 'widget_config_property_name';
-        var label = document.createElement('label');
-        //label.htmlFor = id;
-        label.title = param_options.title;
-        label.appendChild(document.createTextNode(param_options.text));
-        name.appendChild(label);
-        row.appendChild(name);
-        var value = document.createElement('td');
-        value.className = 'config_property_value';
-        var sel = document.createElement('select');
-        if (param_options.title) sel.title = param_options.title;
-        //sel.id = id;
-        var select_options = param_options.options;
-        for (var i=0; i<select_options.length; i++) {
-            var opt = document.createElement('option');
-            opt.value = select_options[i].value;
-            opt.text = select_options[i].text;
-            sel.appendChild(opt);
-        }
-        // set default value of input to value provided in param_current
-        if (param_current) sel.value = param_current;
-        //config_inputs[param_name].element = sel; // add input element to global dict for Save
-        value.appendChild(sel);
-        row.appendChild(value);
-        parent_el.appendChild(row);
-
-        return function () { return sel.value; };
-    }
-
-    function config_number(parent_el, param_name, param_options, param_current) {
-        if (!param_options.type) param_options.type = 'number';
-        return config_string(parent_el, param_name, param_options, param_current);
-    }
-
-    // Return a table row with a simple input field
-    function config_string(parent_el, param_name, param_options, param_current)
-    {
-        var row = document.createElement('tr');
-        // create td to hold 'name' prompt for field
-        var name = document.createElement('td');
-        name.className = 'widget_config_property_name';
-        var label = document.createElement('label');
-        //label.htmlFor = id;
-        label.title = param_options.title;
-        label.appendChild(document.createTextNode(param_options.text));
-        name.appendChild(label);
-        row.appendChild(name);
-        var value = document.createElement('td');
-        value.className = 'config_property_value';
-
-        var input = document.createElement('input');
-
-        if (param_options.type) input.type = param_options.type;
-        if (param_options.step) input.step = param_options.step;
-        if (param_options.title) input.title = param_options.title;
-
-        // set default value of input to value provided in param_current
-        self.log(param_name,'default set to',param_current);
-        if (param_current) input.value = param_current;
-
-        value.appendChild(input);
-        row.appendChild(value);
-
-        parent_el.appendChild(row);
-
-        return function() { return input.value; };
-    }
-
-    // A shim function to provide the 'config cancel' in the active layout
-    function config_shim_cancel(self, config) {
-        // reset original widget background-color to WHITE
-        var widget = document.getElementById(config.widget_id);
-        if (widget) widget.style['background-color'] = 'white';
-
-        // hide the config div
-        var config_div = document.getElementById(config.config_id);
-        config_div.style.display = 'none';
-
-        self.log(config.config_id, 'cancel button');
-    }
-
-    // A shim function to provide the 'config save' in the active layout
-    function config_shim_save(self, config, config_params) {
-        // Here we update the existing widget 'in-place', not expected in production
-
-        params = config_params; //self.params = config_params;
-
-        self.log(config.widget_id,'config reset params to',params);//self.params);
-
-        var widget = document.getElementById(config.widget_id);
-
-        // reset original widget background-color to WHITE
-        widget.style['background-color'] = 'white';
-
-        // hide the config div
-        var config_div = document.getElementById(config_id);
-        config_div.style.display = 'none';
-
-        self.init();
     }
 
     this.log("Instantiated StationBoard", widget_id, params);
