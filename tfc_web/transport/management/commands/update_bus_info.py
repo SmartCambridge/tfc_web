daba443136eee055cbaf216c9172bd5e327e2f53
Abraham Martin <amc203@cam.ac.uk>
2018-04-23 22:18:43 +0100

    add delete layout option to layout_config as well as layout_config_overlay

--- tfc_web/transport/management/commands/update_bus_info.py
+++ tfc_web/transport/management/commands/update_bus_info.py
@@ -85,10 +85,15 @@ class Command(BaseCommand):
                                 end_date=service['OperatingPeriod']['EndDate'],
                                 regular_days_of_week=
                                     list(service['OperatingProfile']['RegularDayType']['DaysOfWeek'].keys())
-                                if 'OperatingProfile' in service and 'RegularDayType' in service['OperatingProfile'] and 'DaysOfWeek' in service['OperatingProfile']['RegularDayType'] else ('MondayToFriday',),
+                                if 'OperatingProfile' in service and
+                                   'RegularDayType' in service['OperatingProfile'] and
+                                   'DaysOfWeek' in service['OperatingProfile']['RegularDayType']
+                                    else ('MondayToFriday',),
                                 bank_holiday_operation=
                                     list(service['OperatingProfile']['BankHolidayOperation']['DaysOfOperation'].keys())
-                                if 'OperatingProfile' in service and 'BankHolidayOperation' in service['OperatingProfile'] and 'DaysOfOperation' in service['OperatingProfile']['BankHolidayOperation'] else None
+                                if 'OperatingProfile' in service and
+                                   'BankHolidayOperation' in service['OperatingProfile'] and
+                                   'DaysOfOperation' in service['OperatingProfile']['BankHolidayOperation'] else None
                             )
 
                             # Routes
