929188c9ccfaea0d9f33c946af25dede508291a7
Jon Warbrick <jw35@cam.ac.uk>
2018-06-01 09:47:08 +0100

    Add link to API from home page

--- tfc_web/api/urls.py
+++ tfc_web/api/urls.py
@@ -5,7 +5,7 @@ from django.views.generic import TemplateView
 from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name="api/index.html")),
+    url(r'^$', TemplateView.as_view(template_name="api/index.html"), name="api_home"),
     # url(r'', include('authmultitoken.endpoint_urls')),
     url(r'', include('authmultitoken.html_urls')),
     url(r'^docs/', include_docs_urls(title='TFC API')),
