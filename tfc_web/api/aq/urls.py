6d298e0fc6636789f02064087f6234af6edcc8f9
Jon Warbrick <jw35@cam.ac.uk>
2018-06-04 14:49:50 +0100

    Rework URLs to be more CoreAPI-friendly
    Move variable components of the URLs to the end, and add a /history/
    component where previously it was assumed based on the presence of
    an ID. This results in rather cleaner CoreAPI 'names' for the various
    endpoints. In summary, from:
    
    /api/v1/aq/{station_id}/{sensor_type}/{month}
    
    /api/v1/parking/{parking_id}
    /api/v1/parking/{parking_id}/latest
    /api/v1/parking/{parking_id}/previous
    
    /api/v1/zone/{zone_id}
    
    to:
    
    /api/v1/aq/history/{station_id}/{sensor_type}/{month}
    
    /api/v1/parking/history/{parking_id}
    /api/v1/parking/latest/{parking_id}
    /api/v1/parking/previous/{parking_id}
    
    /api/v1/zone/history/{zone_id}

--- tfc_web/api/aq/urls.py
+++ tfc_web/api/aq/urls.py
@@ -4,7 +4,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.AQList.as_view()),
-    url(r'^(?P<station_id>[^/]+)/(?P<sensor_type>[^/]+)/(?P<month>\d{4}-\d{2})$', views.AQHistory.as_view()),
     url(r'^(?P<station_id>[^/]+)/$', views.AQConfig.as_view()),
+    url(r'^history/(?P<station_id>[^/]+)/(?P<sensor_type>[^/]+)/(?P<month>\d{4}-\d{2})$', views.AQHistory.as_view()),
+    
 
 ]
--- tfc_web/api/zones/urls.py
+++ tfc_web/api/zones/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.ZoneList.as_view()),
-    url(r'^(?P<zone_id>[^/]+)/history/$', views.ZoneHistory.as_view()),
     url(r'^(?P<zone_id>[^/]+)/$', views.ZoneConfig.as_view()),
+    url(r'^history/(?P<zone_id>[^/]+)$', views.ZoneHistory.as_view()),
 ]
