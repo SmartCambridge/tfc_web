d36c393ea8df605f0f8bfe89d999734f67794981
ijl20 <ijl20@cam.ac.uk>
2018-06-10 19:43:21 +0100

    added wsgi_dev.py for dev gunicorn start

---
+++ tfc_web/tfc_web/settings_devijl.py
@@ -0,0 +1,15 @@
+# ijl20 dev settings
+# as production except DEBUG=true and no https redirect
+
+from tfc_web.settings import *
+
+DEBUG = True
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+
+DEFAULT_FROM_EMAIL = "smart-cambridge@cl.cam.ac.uk"
+EMAIL_HOST = "ppsw.cam.ac.uk"
+EMAIL_PORT = 25
+
+TFC_SERVER_CSN_API = "http://localhost/httpmsg/A/tfc.manager/msgrouter/A"
