9036278887c64d6a042bec33334d6bdac5e093a6
Abraham Martin <amc203@cam.ac.uk>
2018-06-12 22:05:56 +0100

    unused URLs

--- tfc_web/transport/urls.py
+++ tfc_web/transport/urls.py
@@ -43,8 +43,8 @@ urlpatterns = [
     # Bus Lines
     url(r'^lines/$', views.bus_lines_list, name='bus-lines-list'),
     url(r'^lines/area/(?P<area_id>\d+)/$', views.bus_lines_list, name='bus-lines-list-area'),
-    url(r'^line/(?P<bus_line_id>.+)/$', views.bus_line_timetable, name='bus-line'),
-    url(r'^line/old/(?P<bus_line_id>.+)/$', views.bus_line, name='bus-line-old'),
+#    url(r'^line/(?P<bus_line_id>+)/$', views.bus_line_timetable, name='bus-line'),
+#    url(r'^line/old/(?P<bus_line_id>.+)/$', views.bus_line, name='bus-line-old'),
 
     # Bus Routes
     url(r'^route/map/(?P<bus_route_id>.+)/$', views.bus_route_map, name='bus-route-map'),
