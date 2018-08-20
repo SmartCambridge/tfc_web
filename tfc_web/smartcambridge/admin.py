d77c8fce4c50c9ea5a4d01156d6c32746e1aa979
Jon Warbrick <jw35@cam.ac.uk>
2018-08-20 13:56:20 +0100

    make SmartCambridgeUser work better with Django Admin
    Add a __str__ method to SmartCambridgeUser, and add 'Accepted TCs'
    to the list view in Django Admin.

--- tfc_web/smartcambridge/admin.py
+++ tfc_web/smartcambridge/admin.py
@@ -2,5 +2,8 @@ from django.contrib import admin
 from django.contrib.admin import ModelAdmin
 from smartcambridge.models import SmartCambridgeUser
 
+class SmartCambridgeUserAdmin(admin.ModelAdmin):
+    list_display = ('user', 'accepted_tcs')
+    ordering = ('user',)
 
-admin.site.register(SmartCambridgeUser, ModelAdmin)
+admin.site.register(SmartCambridgeUser, SmartCambridgeUserAdmin)
