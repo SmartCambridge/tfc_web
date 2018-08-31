4bd094273daa4b23ffc42c25e14d58a27d92f7d8
Jon Warbrick <jw35@cam.ac.uk>
2018-08-31 17:04:40 +0100

    Add missing trailing '/' on some endpoints
    The URLs for the API endpoints were inconsistent in their use
    of trailing slashes. This edit rationalises the situation
    so that trailing slashes are always included.

--- tfc_web/aq/api/urls.py
+++ tfc_web/aq/api/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.AQList.as_view()),
     url(r'^(?P<station_id>[^/]+)/$', views.AQConfig.as_view()),
-    url(r'^history/(?P<station_id>[^/]+)/(?P<sensor_type>[^/]+)/(?P<month>[^/]+)$', views.AQHistory.as_view()),
+    url(r'^history/(?P<station_id>[^/]+)/(?P<sensor_type>[^/]+)/(?P<month>[^/]+)/$', views.AQHistory.as_view()),
 ]
--- tfc_web/traffic/api/urls.py
+++ tfc_web/traffic/api/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.ZoneList.as_view()),
     url(r'^(?P<zone_id>[^/]+)/$', views.ZoneConfig.as_view()),
-    url(r'^history/(?P<zone_id>[^/]+)$', views.ZoneHistory.as_view()),
+    url(r'^history/(?P<zone_id>[^/]+)/$', views.ZoneHistory.as_view()),
 ]
