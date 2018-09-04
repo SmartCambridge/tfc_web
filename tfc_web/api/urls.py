da70d1aa88c06aa9236306e54fcd3fcfc0507e54
Jon Warbrick <jw35@cam.ac.uk>
2018-09-04 16:53:18 +0100

    Fixup API URL's broken in previous edit

--- tfc_web/api/urls.py
+++ tfc_web/api/urls.py
@@ -10,14 +10,15 @@ from .api_docs import api_description
 # These are the URL patterns for which documentation should
 # be generated
 docpatterns = [
-    url(r'^/api/v1/parking/', include('parking.api.urls')),
-    url(r'^/api/v1/zone/', include('traffic.api.urls')),
-    url(r'^/api/v1/aq/', include('aq.api.urls')),
+    url(r'^api/v1/parking/', include('parking.api.urls')),
+    url(r'^api/v1/zone/', include('traffic.api.urls')),
+    url(r'^api/v1/aq/', include('aq.api.urls')),
     # Import transport views previously served under /transport/api/
-    url(r'^/api/v1/transport/', include('transport.api.urls')),
+    url(r'^api/v1/transport/', include('transport.api.urls')),
 ]
 
-# These are all the URLs
+# These are all the URLs. The near-duplication of the URLs above
+# is regretted
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name="api/index.html")),
     # url(r'', include('authmultitoken.endpoint_urls')),
@@ -26,5 +27,10 @@ urlpatterns = [
     url(r'^docs/', include_docs_urls(
         title='SmartCambridge API',
         description=api_description,
-        patterns=docpatterns))
-    ] + docpatterns
+        patterns=docpatterns)),
+    url(r'^v1/parking/', include('parking.api.urls')),
+    url(r'^v1/zone/', include('traffic.api.urls')),
+    url(r'^v1/aq/', include('aq.api.urls')),
+    # Import transport views previously served under /transport/api/
+    url(r'^v1/transport/', include('transport.api.urls')),
+    ]
