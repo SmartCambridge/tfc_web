7de0a586cd1a685059d015b386e8df609af01322
Jon Warbrick <jw35@cam.ac.uk>
2018-12-11 16:39:37 +0000

    Suppress extensive debugging

--- tfc_web/smartpanel/static/smartpanel/pocket.js
+++ tfc_web/smartpanel/static/smartpanel/pocket.js
@@ -7,8 +7,8 @@
 'use strict';
 
 // Widget spec requires a DEBUG global (even if empty)
-// var DEBUG = '';
-var DEBUG = 'weather_log station_board_log stop_timetable_log stop_bus_map_log rtmonitor_api_log';
+var DEBUG = '';
+// var DEBUG = 'weather_log station_board_log stop_timetable_log stop_bus_map_log rtmonitor_api_log';
 // var DEBUG = 'rtmonitor_api_log';
 
 // Version number of the agreed TCs
