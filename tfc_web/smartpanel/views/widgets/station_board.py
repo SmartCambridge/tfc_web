5856990fde8490b32f66a6cbdb1a020f40955e4b
ijl20 <ijl20@cam.ac.uk>
2018-12-14 10:34:02 +0000

    smartpanel station_board.py local cached WSDL files

--- tfc_web/smartpanel/views/widgets/station_board.py
+++ tfc_web/smartpanel/views/widgets/station_board.py
@@ -2,12 +2,14 @@ import logging
 from django.core.cache import cache
 from django.shortcuts import render
 from django.conf import settings
+from django.templatetags.static import static
 import zeep
 import re
 
 logger = logging.getLogger(__name__)
 
-WSDL = 'https://lite.realtime.nationalrail.co.uk/OpenLDBWS/wsdl.aspx?ver=2017-10-01'
+# WSDL = 'https://lite.realtime.nationalrail.co.uk/OpenLDBWS/wsdl.aspx?ver=2017-10-01'
+WSDL = static('smartpanel/widgets/station_board/OpenLDBWS_wsdl_2017-10-01.xml')
 
 STATION_ABBREV = {
   'London Kings Cross': 'London Kings X',
@@ -34,7 +36,15 @@ def station_board(request):
         logger.info('Cache miss for %s', cache_key)
         data = {'messages': [], 'services': []}
 
-        client = zeep.Client(wsdl=WSDL)
+        protocol = 'http://'
+        if request.is_secure():
+            protocol = 'https://'
+
+        hostname = request.get_host()
+
+        absolute_wsdl = protocol + hostname + WSDL
+
+        client = zeep.Client(wsdl=absolute_wsdl)
         raw_data = client.service.GetDepartureBoard(
             numRows=50, crs=station,
             _soapheaders={"AccessToken": settings.NRE_API_KEY},
