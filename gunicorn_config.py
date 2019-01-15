49478be87e133ea590bb130aa0c9873a948cbdcd
Jon Warbrick <jw35@cam.ac.uk>
2019-01-15 11:24:05 +0000

    Add statsd logging config parameters
    Add statsd logging parameters to send metrics to Prometheus
    (see https://github.com/SmartCambridge/tfc_prod/tree/master/system_metrics)
    
    Note that the prefix "tfc_web" is copied through as the 'prefix' tag
    in the collected Prometheus metrics.

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -12,3 +12,6 @@ capture_output = "True"
 errorlog = "/var/log/tfc_prod/gunicorn.err"
 accesslog = "/var/log/tfc_prod/gunicorn.log"
 access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s" %(D)s'
+
+statsd_host = "localhost:9125"
+statsd_prefix = "tfc_web"
