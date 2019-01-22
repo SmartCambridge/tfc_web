8527864d906df243909e9c70aef7a9f04ceba3bf
Abraham Martin <amc203@cam.ac.uk>
2019-01-22 22:34:45 +0000

    Import changes from https://github.com/SmartCambridge/BusStopChooser/pull/6

--- tfc_web/smartpanel/static/smartpanel/bus_stop_chooser/bus_stop_chooser.js
+++ tfc_web/smartpanel/static/smartpanel/bus_stop_chooser/bus_stop_chooser.js
@@ -119,10 +119,8 @@ var BusStopChooser = (function() {
             var multi_select = params.multi_select || false;
             var popups = params.popups || false;
             var location = params.location || false;
-            // If onclick_redirect is set to true, uses an alternative on_click event
-            // that will redirect the user to the bus stop web app endpoint.
-            var onclick_redirect = params.onclick_redirect || false;
             var zoom_threshold = params.zoom_threshold || 15;
+            var on_click_stop_callback = params.on_click_stop_callback || undefined;
             var stops_callback = params.stops_callback || undefined;
             var api_endpoint = params.api_endpoint || DEFAULT_ENDPOINT;
             var api_token = params.api_token;
@@ -133,7 +131,9 @@ var BusStopChooser = (function() {
                 maxZoom: OSM_MAX_ZOOM
             });
             var selected_stops = L.featureGroup();
-            var other_stops = L.markerClusterGroup();
+            var other_stops = L.markerClusterGroup({
+                disableClusteringAtZoom: 18
+            });
 
             var warning_div = document.createElement('div');
             warning_div.className = 'bus_stop_chooser_warning';
@@ -245,7 +245,6 @@ var BusStopChooser = (function() {
 
             function process_pan_and_zoom(e) {
                 // Handler for pan and zoom ('moveend') events
-
                 debug_log('Processing pan and zoom, arg', e);
                 spinner_img.style.display = 'block';
                 if (map.getZoom() < zoom_threshold) {
@@ -275,7 +274,6 @@ var BusStopChooser = (function() {
                         });
                     }
                 }
-
             }
 
 
@@ -326,6 +324,7 @@ var BusStopChooser = (function() {
 
             }
 
+
             var safe_get_bus_stops = debounce(get_bus_stops, 750);
 
 
@@ -368,7 +367,7 @@ var BusStopChooser = (function() {
 
                     debug_log('Adding', stop.stop_id);
                     var marker = L.marker([stop.lat, stop.lng])
-                        .on('click', onclick_redirect ? process_stop_click_redirect : process_stop_click);
+                        .on('click', process_stop_click);
                     if (popups) {
                         marker.bindPopup(formatted_stop_name(stop.indicator, stop.common_name));
                     } else {
@@ -408,14 +407,12 @@ var BusStopChooser = (function() {
                         selected = true;
                     }
                 });
-                debug_log('Selected', selected);
 
-                if (selected) {
-                    deselect_stop(clicked_marker);
+                if (on_click_stop_callback) {
+                    on_click_stop_callback(clicked_marker, selected);
                 }
-                else {
-                    select_stop(clicked_marker);
-                }
+
+                select_or_deselect_stop(clicked_marker, selected);
 
                 do_stops_callback();
 
@@ -423,37 +420,24 @@ var BusStopChooser = (function() {
 
             }
 
-
-            function process_stop_click_redirect(e) {
-                window.location.href = '/transport/stop/' + e.target.properties.stop.stop_id;
-            }
-
-
-            function select_stop(marker) {
-                // First remove anything currently selected if not multi_select
-                // [*should* only ever be one, but who knows?]
-                if (!multi_select) {
-                    selected_stops.eachLayer(function(m) {
-                        deselect_stop(m);
-                    });
+            function select_or_deselect_stop(marker, selected) {
+                if (selected) {
+                    // First remove anything currently selected if not multi_select
+                    // [*should* only ever be one, but who knows?]
+                    if (!multi_select) {
+                        selected_stops.eachLayer(function(m) {
+                            deselect_stop(m);
+                        });
+                    }
+                    other_stops.removeLayer(marker);
+                    marker.addTo(selected_stops);
+                    marker.setIcon(stop_icon_selected);
+                } else {
+                    selected_stops.removeLayer(marker);
+                    marker.addTo(other_stops);
+                    marker.setIcon(stop_icon);
                 }
-                other_stops.removeLayer(marker);
-                marker.addTo(selected_stops);
-                marker.setIcon(stop_icon_selected);
-                if (popups) {
-                    //marker.openPopup();
-                }
-                debug_log('Selected', marker);
-            }
-
-            function deselect_stop(marker) {
-                selected_stops.removeLayer(marker);
-                marker.addTo(other_stops);
-                marker.setIcon(stop_icon);
-                if (popups) {
-                    //marker.openPopup();
-                }
-                debug_log('Deselected', marker);
+                debug_log(selected ? 'Selected' : 'Deselected', marker);
             }
 
             function list_selected_stops() {
